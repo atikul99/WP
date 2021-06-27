f5f3c0fb23ef8a4643f191953df5f44849a56784
Atikul Islam <53938629+atikul99@users.noreply.github.com>
2021-06-27 12:42:58 +0600

    Update post-type.php

--- post-type.php
+++ post-type.php
@@ -1,1 +1,17 @@
 
+				
+// Display Custom post type category
+
+<?php while ($the_query->have_posts()) : $the_query->the_post(); 						
+  $terms = get_the_terms(get_the_ID(), 'em_portfolio_cat');
+?>
+
+
+<p>
+  <?php if( $terms ){
+    foreach( $terms as $single_slugs ){?>
+      <span class="category-item-p">
+        <?php echo $single_slugs->name ;?>
+      </span>																			
+  <?php }}?>
+</p>
